67bbbf48f85f2f5111e4ec90925cb210155fc3ff
tllach <tllach@uninorte.edu.co>
2023-04-29 21:22:31 -0500

    minor changes to fix a error

--- Homepage.py
+++ Homepage.py
@@ -65,10 +65,7 @@ if buttonSearch:
     rows = run_query("select * from DatosCovid as dc where dc.Date >= '2020-01-12' and dc.Date < '2020-01-13';")
     # Print results.
     for row in rows:
-        for i in range(row):
-            st.write(row[i], end="")
-        st.write("")
-        #st.write(f"{row[0]}{row[1]}{row[2]}{row[3]}:")
+        st.write(f"{row[0]}{row[1]}{row[2]}{row[3]}{row[4]}{row[5]}{row[6]}")
         
 
 
