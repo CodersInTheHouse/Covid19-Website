d483be52f5a1d229c9582c107ea5cfb4ae4c4cc4
adcerro <79855505+adcerro@users.noreply.github.com>
2023-05-01 23:44:53 -0500

    Feat graphics (#2)
    * First attempt with queries for Graphics
    
    * feat: tab2 graphics works nice
    
    * fix: variables now taken into account
    
    * fix:replaced match for backwards compatibility
    
    * removed positive rate from list
    
    * fix: changed linechart and vaccinated chart
    
    * fix: changed typo
    
    * final form

--- Homepage.py
+++ Homepage.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pyodbc
 from datetime import date, timedelta
+import numpy as np
 
 
 st.set_page_config(page_title= "Covid-19 Data", page_icon=":bar_chart:", layout="wide")
@@ -11,19 +12,32 @@ st.set_page_config(page_title= "Covid-19 Data", page_icon=":bar_chart:", layout=
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return pyodbc.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
+    connected = False
+    while (connected==False):
+        conn = pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+            + st.secrets["server"]
+            + ";DATABASE="
+            + st.secrets["database"]
+            + ";UID="
+            + st.secrets["username"]
+            + ";PWD="
+            + st.secrets["password"],
+            timeout=30
+        )
+        try:
+            connected = True
+            conn.cursor().execute('select top 1 from DatosCovid')
+        except pyodbc.Error as error:
+            if error.args[0] == "08S01":
+                connected= False
+                conn.close()
+    return conn
+
+
+
 
 conn = init_connection()
-
 
 #SIDE BAR 
 st.sidebar.header('Please choose the items below:')
@@ -35,11 +49,18 @@ country = st.sidebar.multiselect(
 
 #SELECT VARIABLES
 variables = st.sidebar.selectbox(
-    'Variables', ('Confirmed cases', 'Confirmed deaths', 'Fully vaccunated', 'ICU patient', 'Positive test' )
+    'Variables', ('Confirmed cases', 'Confirmed deaths', 'Fully vaccinated', 'ICU patient' )
 )
 
-#BUTTON
-buttonSearch = st.sidebar.button('Search')
+if (variables=="Confirmed cases"):
+    rVar='total_cases'
+elif (variables=="Confirmed deaths"):
+    rVar='total_deaths'
+elif (variables=="Fully vaccinated"):
+    rVar='people_fully_vaccinated'
+else:
+    rVar='icu_patients'
+
 
 date_inicio = date(2020, 3, 10) #date inicio pandemia
 date_fin = date(2023,1,21) #date fin pandemia
@@ -55,6 +76,8 @@ end_date = st.sidebar.date_input('End Date',
                                  min_value= date_inicio, 
                                  max_value= date_fin)
 
+#BUTTON
+buttonSearch = st.sidebar.button('Search')
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -64,6 +87,10 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
+@st.cache_data(ttl=300)
+def run_queryDF(query):
+    return pd.read_sql(query,conn)
+
 def createSentence(lista: list):
     sentence = f"('{lista[0]}'"
     if len(lista) > 1:
@@ -72,26 +99,76 @@ def createSentence(lista: list):
     sentence += ")"
     return sentence
 
+def prettySentence(list:list):
+    text = ','.join(list)
+    if(len(list)>1):
+        text = text.replace(f',{list[-1]}',f' and {list[-1]}')
+    return text
+
 def isAllCorrect() -> bool:
     return len(country) > 0
 
+#Tabs
+tab0, tab1, tab2 = st.tabs(['Query','Line','Bars'])
 
 if buttonSearch:
     if isAllCorrect():
         #Funcion que retorna los paises seleccionados en forma de query
         sentence = createSentence(country)
+        pSentence = prettySentence(country)
         
-        rows = run_query(f"select * from DatosCovid as dc where dc.Date >= '{start_date}' and dc.Date <= '{end_date}' and dc.Location IN {sentence};")
+        #rows = run_query(f"select * from DatosCovid as dc where dc.Date >= '{start_date}' and dc.Date <= '{end_date}' and dc.Location IN {sentence};")
+        
         # Print results.
-        for row in rows:
-            st.write(f"{row[0]} - {row[1]} - {row[2]} - {row[3]} - {row[4]}")
+        #for row in rows:
+        #    st.write(f"{row[0]} - {row[1]} - {row[2]} - {row[3]} - {row[4]}")
+        with tab0:
+            data = run_queryDF(f"select dc.Location,dc.Date,dc.{rVar} from DatosCovid as dc where dc.Date >= '{start_date}' and dc.Date <= '{end_date}' and dc.Location IN {sentence};")
+            st.table(data)
+
+        with tab1:
+            st.subheader(f'Linechar for: {pSentence}')
+            if len(country)>1:
+                st.warning("Sorry, this chart can only handle one country at a time 🧐🧐")                
+            else:
+                #print(f"select month(dc.Date) as mes,max(dc.{rVar}) as total from DatosCovid dc where dc.Location IN {sentence} group by month(dc.Date) order by  month(dc.Date) asc")
+                if(rVar=='people_fully_vaccinated'):
+                    data1 = run_queryDF(f"select cast(year(dc.Date) as varchar) + '-' + cast(month(dc.Date) as varchar) as mes, max(dc.people_fully_vaccinated) as total from DatosCovid dc where dc.Location IN {sentence} and dc.Date >= '{start_date}' and dc.Date <= '{end_date}'group by month(dc.Date),year(dc.Date), dc.people_fully_vaccinated order by  year(date),month(dc.Date) asc")
+                    st.line_chart(data=data1, y='total')
+                else:
+                    data1 = run_queryDF(f"select month(dc.Date) as mes,max(dc.{rVar}) as total from DatosCovid dc where dc.Location IN {sentence} and dc.Date >= '{start_date}' and dc.Date <= '{end_date}' group by month(dc.Date) order by  month(dc.Date) asc")
+                    st.line_chart(data=data1,x='mes', y='total')
+
+            
+
+        with tab2:
+            st.subheader(f'Barchar for: {pSentence}')
+            if len(country)>1:
+                data2 = run_queryDF(f"select dc.Location, max(dc.{rVar}) as total from DatosCovid dc where dc.Location in {sentence} and dc.Date >= '{start_date}' and  dc.Date <= '{end_date}' group by dc.Location")
+                st.bar_chart(data=data2,x='Location', y='total')
+            else:
+                data2 = run_queryDF(f"select year(dc.Date) as año, max(dc.{rVar}) as total from DatosCovid dc where dc.Location in ('Canada') and dc.Date >= '{start_date}' and  dc.Date <= '{end_date}' group by year(dc.Date) order by year(dc.Date) asc")
+                st.bar_chart(data=data2,x='año', y='total')
+
     else:
         st.warning("Sorry, there's been a problem filling those boxes 🧐🧐")
         st.info("Please check and hit that search button again! 🤞")
 else:
-    st.info("Welcome to our webpage!")
-    st.write("Here you can see really interesenting graphs related to COVID-19")
-    st.success("To visualizes the graphics. Fill the boxes on your left 👈")
+    with tab0:
+        st.info("Welcome to our webpage!")
+        st.write("Here you can see really interesenting graphs related to COVID-19")
+        st.success("To visualizes the graphics. Fill the boxes on your left 👈")
+    
+    with tab1:
+        st.info("Here you will see a line chart of the query you just performed!")
+    
+    with tab2:
+        st.info("Here you will see a bar chart of the query you just performed!")
+    
+
+
+
+    
 
 #HIDE LINES MENU AND MADE WITH STREAMLIT 
 hide_st_style = """
---
+++ README.md
@@ -0,0 +1,69 @@
+# Covid-19 Data presentation
+> This is the lab project for the 2023-10 databases class.
+> Live demo [_here_](http://covid19-website.streamlit.app/). <!-- If you have the project hosted somewhere, include the link here. -->
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+<!-- * [License](#license) -->
+
+
+## General Information
+- Provide general information about your project here.
+- What problem does it (intend to) solve?
+- What is the purpose of your project?
+- Why did you undertake it?
+<!-- You don't have to answer all the questions - just the ones relevant to your project. -->
+
+
+## Technologies Used
+- Python - version 3.11
+- Streamlit - version 1.21.0
+- pyodbc - version 4.0.39
+
+
+## Features
+List the ready features here:
+- It kinda works!
+- A side bar
+- it uses an sql database
+
+
+## Usage
+Execute with streamlit
+
+`stremlit run Hompeage.py`
+
+
+## Project Status
+Project is: _complete_ <!-- / _complete_ / _no longer being worked on_. If you are no longer working on it, provide reasons why. --> 
+
+
+## Room for Improvement
+
+Room for improvement:
+- Time managment by alan
+- Pretty charts
+
+
+## Acknowledgements
+Give credit here.
+- This project was inspired by Jimeno
+- Many thanks to Tabata & Katy
+
+
+## Contact
+Created by [CodersInTheHouse](https://github.com/CodersInTheHouse) - feel free to contact us!
+
+
+<!-- Optional -->
+<!-- ## License -->
+<!-- This project is open source and available under the [... License](). -->
+
+<!-- You don't have to include all sections - just the one's relevant to your project -->
